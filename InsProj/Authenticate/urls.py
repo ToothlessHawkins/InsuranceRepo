3028e0a7445f48f66f77c367c3c052515277648b
Justin John <jxj7701@gmail.com>
2018-02-14 09:46:36 -0500

    logout button added to base.html
    added django default logout in authenticate urls.py, linked to from base.html so all pages that draw from that can log out

--- InsProj/Authenticate/urls.py
+++ InsProj/Authenticate/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from Authenticate.views import signUp,logIn
 
 app_name='Authenticate'
 urlpatterns = [
     url('signup/',signUp,name = 'signup'),
-    url('login/',logIn,name='login')
+    url('login/',logIn,name='login'),
+    path('logout/', auth_views.logout, {'next_page': 'Authenticate:login'}, name='logout'),
 ]
