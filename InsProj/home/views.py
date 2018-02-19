5006a13343d7a80e922c25cb215d52ae6d7bc964
RannellRobertson <rrrprobertson@gmail.com>
2018-02-19 16:31:37 -0500

    Modifications to home and policy app
    Changed due_payments template and created a policy_confirmation template.
    Edited home.views and policy.views.

--- InsProj/home/views.py
+++ InsProj/home/views.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render_to_response,render, get_object_or_404
 from policies.models import policy
 from django.conf import settings
-# from accounts.models import account
+from accounts.models import account
 from django.http import HttpResponseRedirect
-# from .forms import MakePaymentForm
+import datetime
 from django.urls import reverse
 from paypal.standard.forms import PayPalPaymentsForm
 
 # Create your views here.
 def customer_home_page(request):
+    currentUser = request.user.username
     # username = account.objects.get(user_name=user_name)
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'currentUser' : currentUser})
 
 def view_payment_history(request, pk):
     username = get_object_or_404(policy, pk=pk)
@@ -21,12 +22,12 @@ def view_payment_history(request, pk):
 
 def make_payment(request):
     policy_id = request.session.get('policy_id')
-    policy_plan = policy.objects.get(policy, id=policy_id)
+    policy_plan = get_object_or_404(policy, policy_id=policy_id)
     host = request.get_host()
 
     paypal_dict = {
         'business': settings.PAYPAL_RECEIVER_EMAIL,
-        'amount': '{}'.format(policy.total_rate),
+        'amount': '{}'.format(policy.balance),
         'policy': 'Policy {}'.format(policy.policy_id),
         'invoice': str(policy.policy_id),
         'currency_code': 'USD',
@@ -39,13 +40,49 @@ def make_payment(request):
                                                          'form': form})
 
 def payment_confirmation(request):
-    return render(request, 'payment_confirmation.html', {})
+    currentUser = request.user.username
+    currentAcc = account.objects.get(user_name=currentUser)
+    activePolicy = policy.objects.get(account=currentAcc)
+    activePolicy.points += 1
+    #increment payments_made
+    activePolicy.payments_made += 1
+    #figure out total number of payments based on payment plan
+    #if there are more payments to be made if(total-paymments > 0)
+    #set balance equal to new amount
+    #increment due date based on payment plan
+    activePolicy.balance = 0
+    totalPayments = 12 if policy.payment_plan == 'M' else 52 if policy.payment_plan == 'W' else 1
+    # time = datetime.week if activePolicy.payment_plan == 'W' else datetime.month if activePolicy.payment_plan == 'M' else datetime.MAXYEAR
+    if activePolicy.payments_made == totalPayments:
+        activePolicy.payments_made == 0
+    else:
+        activePolicy.balance = activePolicy.total_rate / totalPayments
 
-def on_time_payments(request, pk):
-    username = policy.objects.get(pk=pk)
-    return render(request, 'on-time_payments', {'username' : username})
 
-def due_payments(request, pk):
-    username = policy.objects.get(pk=pk)
-    username.points += 1
-    return render(request, 'due_payments', {'username' : username})
+    return render(request, 'payment_confirmation.html', {'currentUser' : currentUser,
+                                                         'currentAcc' : currentAcc,
+                                                         'activePolicy' : activePolicy,})
+
+def on_time_payments(request):
+    currentUser = request.user.username
+    currentAcc = account.object.get(user_name=currentUser)
+    activePolicy = policy.objects.get(account=currentAcc)
+    return render(request, 'on-time_payments', {'currentUser' : currentUser,
+                                                'activePolicy' : activePolicy,
+                                                'activePolicy' : activePolicy})
+
+def due_payments(request):
+    # username = policy.objects.get(pk=policy_id)
+    currentUser = request.user.username
+    currentAcc = account.objects.get(user_name=currentUser)
+    activePolicy = policy.objects.get(account=currentAcc)
+    #time = datetime.date.today() + datetime.timedelta(week=1) if activePolicy.payment_plan == 'W' else datetime.date.today() + datetime.timedelta(month=1) if activePolicy.payment_plan == 'M' else datetime.date.today()
+    #account associated with username
+    #active policy of that account
+    due = False
+    if datetime.date.today() > activePolicy.payment_due_date - datetime.timedelta(weeks=1):
+        due = True
+
+    return render(request, 'due_payments.html', {'currentUser' : currentUser,
+                                                 'activePolicy' : activePolicy,
+                                                 'due' : due})
--- InsProj/policies/views.py
+++ InsProj/policies/views.py
@@ -11,8 +11,8 @@ from accounts.models import account as acc
 
 # Create your views here.
 def view_holder_policies(request, policy_id):
-    username = policy.objects.get(policy_id=policy_id)
-    return render(request, 'view_holder_policies', {'username' : username})
+    yourPolicy = policy.objects.get(policy_id=policy_id)
+    return render(request, 'view_holder_policies.html', {'yourPolicy' : yourPolicy})
 
 def view_policy_details(request, first_name):
     policy_info = policy.objects.get(first_name=first_name)
@@ -23,18 +23,15 @@ def new_policy(request):
         form = PolicyForm(request.POST)
         if form.is_valid():
             policy = form.save(commit=False)
-            currentUserAcc = acc.objects.get(user_name=request.user.username)
-            policy.account = currentUserAcc
+            policy.account = acc.objects.get(user_name=request.user.username)
             # time = datetime.date.month if policy.payment_plan == 'M' else datetime.date. policy.payment_plan == 'Y'
             # policy.payment_due_date = policy.start_date + time
             policy.payment_due_date = datetime.date.today()
             div = 12 if policy.payment_plan == 'M' else 52 if policy.payment_plan =='W' else 1
             policy.balance = policy.total_rate / div
             policy.save()
-            currentUserAcc.active_policy = policy.policy_id
-            currentUserAcc.save()
-            return HttpResponseRedirect(reverse('home:customer_home_page', kwargs={'user_name':request.user.username}))
+            return HttpResponseRedirect(reverse('accounts:policy_confirmation'))
     else:
         form = PolicyForm()
 
-    return render(request, 'policies/policy_select.html', {'form':form})
+    return render(request, 'new_policy.html', {'form':form})
